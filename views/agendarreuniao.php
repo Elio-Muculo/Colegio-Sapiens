39151a329dcece27f414a3aedf03caf943530920
filipeManjate <87928895+filipeManjate@users.noreply.github.com>
2022-09-25 23:50:05 +0200

    adicao de cor e alogamento da barra

--- views/agendarreuniao.php
+++ views/agendarreuniao.php
@@ -6,13 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agendar Reunião</title>
-    <link rel="stylesheet" href="../assets/cssgeral/edmilson/style.css">
+   
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    
+    <link rel="stylesheet" href="../assets/cssgeral/edmilson/style.css">
 </head>
 
 <body>
+
+                    <div class="alinhamento">
+                            <div class="alinhamnento4">
+                               cSP 
+                            </div>
+                                 
+                            <div class="alinhamento3">
+                             
+                                    Nome do Professor
+                                
+                            </div>
+
+                    </div>
+
     <div class="container-fluid">
         <div class="centro w-40">
             <div class="w-40">
@@ -22,20 +36,11 @@
                     <div class="telaP col-md-9 col-lg-9 col-sm-12 px-auto mx-auto">
 
 
-                    <div class="alinhamento">
-                            <div class="alinhamnento4">
-                               cSP 
-                            </div>
-                            <div class="alinhamento3">
-                                Nome do Professor
-                            </div>
-
-                    </div>
             
                         <h1 class="texto text-center mt-5">Agendar Reunião</h1>
-                        <form action="controller/agendarreuniao.php" method="post" class=" row p-3">
+                        <form action="controller/" method="post" class=" row p-3">
                             
-                <div class="alinhamento1" >
+                <div class="alinhamento1" tyle="background-color: #d9d9d9"  >
 
                  
 <p></p>
@@ -43,7 +48,7 @@
                             <div class=" col-md-4 col-lg-4 col-sm-12">
                                 <div class="input-group mb-3" style="width: 185%">
                                     <span class="input-group-text"><i class="fa-solid fa-calendar"></i></span>
-                                    <input type="date" class="form-control" name="date"  style="width: 80%">
+                                    <input type="date" class="form-control" name="dete" placeholder="data da Reunião" style="width: 80%">
                                 </div>
                             </div>
 
@@ -51,7 +56,7 @@
 
                             <div class="col-md-4 col-lg-4 col-sm-12">
                                 <div class="input-group mb-3" style="width: 185%">
-                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    <span class="input-group-text"><i class="fas fa-user"></i></i></span>
                                     <select class="form-select" name="participantes" placeholder="Selecione os Participantes" style="width: 80%">
                                         <option selected>Selecione Participantes</option>
                                         <option>Turma A</option>
@@ -63,7 +68,7 @@
                             
                             <div class=" col-md-4 col-lg-4 col-sm-12">
                                 <div class="input-group mb-3" style="width: 185%">
-                                    <span class="input-group-text"><i class="fa-regular fa-pen-nib"></i></span>
+                                    <span class="input-group-text"><i class="fa-regular fa-calendar-check"></i></span>
                                     <input type="text" class="form-control" name="assunto" placeholder="Assunto da Reunião" style="width: 80%">
                                 </div>
                             </div>
@@ -71,11 +76,11 @@
 
                             <div class="col-md-4 col-lg-4 col-sm-12">
                                 <div class="input-group mb-3" style="width: 185%">
-                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    <span class="input-group-text"><i class="fa-duotone fa-person-military-to-person"></i></span>
                                     <select class="form-select" name="tipo" style="width: 80%">
                                         <option selected>Tipo de Reunião</option>
-                                        <option>Urgente</option>
-                                        <option>Trimestral</option>
+                                        <option>Ordinaria</option>
+                                        <option>Extra-ordinaria</option>
                                     </select>
                                 </div>
                             </div>
@@ -88,12 +93,12 @@
 <br>     
                      
                         
-                            <div class="col-md-3 col-lg-3 col-sm-12 botao text-center " style="margin-left: 25%">
+                            <div class="col-md-3 col-lg-3 col-sm-12 botao text-center " style="margin-left: 27%">
                                 <button type="submit" class="btn"  >Agendar</button>
                             </div>
 
-                            <div class="col-md-3 col-lg-3 col-sm-12 botao text-center mx-3" style="margin-left: 75%">
-                                <button class="btn" >Cancelar</button>
+                            <div class="col-md-3 col-lg-3 col-sm-12 botao text-center mx-3" style="margin-left: 77%">
+                                <button class="btn" >Canselar</button>
                             </div>
                        
                    
