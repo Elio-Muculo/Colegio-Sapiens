d81f973b49414f638ae0b6ba67f0b2887163a0b9
Elliot <emuculo25@gmail.com>
2022-09-26 01:00:43 +0200

    email com assunto da reuniao

--- controller/agendarreuniao.php
+++ controller/agendarreuniao.php
@@ -1,4 +1,17 @@
 <?php 
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+
 session_start();
 require_once '../config/crud.php';
 
@@ -17,9 +30,50 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         'professor_cod' => $professor['codigo'],
         'encarregado_cod' => $_POST['encarregado_cod']
     ];
+
+    $encarregado = readOne("SELECT * FROM encarregado WHERE codigo = :id", ['id' => $_POST['encarregado_cod']]);
     
     $inserted = insertAll("INSERT INTO reuniao (tipo, assunto, data, professor_cod, encarregado_cod) VALUES (:tipo, :assunto, :data, :professor_cod, :encarregado_cod)", $dadosReuniao);
     if ($inserted == 1) {
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'emuculo25@gmail.com';                     //SMTP username
+            $mail->Password   = 'zfnsbwzonjjxkchs';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            $mail->SMTPOptions = array(
+            'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+            )
+            );
+            //Recipients
+            $mail->setFrom('emuculo25@gmail.com', $professor['nome']);
+            $mail->addAddress($encarregado['email'], $encarregado['nome']);     //Add a recipient
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = "Reuniao [". $dadosReuniao['tipo'] ."] " . $dadosReuniao['assunto'];
+            $mail->Body    = '<strong>Saudações caro encarregado</strong>, recebeu esse e-mail para que compareça <br> a uma reunião agendada para o dia ' . $dadosReuniao['data'] . ' <br>com o seguinte assunto ' . $dadosReuniao['assunto'] . ' aguardamos a sua presença. <br><br><p> Melhores cumprimentos </p>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+
+
         die("dado inserido com sucesso");
         header('Location: ../views/admin.php');
         die("dado inserido com sucesso");
@@ -31,4 +85,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
     
+
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f87ea0fc48c64ba9c711972c45d75a57b489a3b9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f87ea0fc48c64ba9c711972c45d75a57b489a3b9',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.4',
+            'version' => '6.6.4.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'a94fdebaea6bd17f51be0c2373ab80d3d681269b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
