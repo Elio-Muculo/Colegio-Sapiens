15ff23e56303c3eec2da6300245da06509438933
filipeManjate <87928895+filipeManjate@users.noreply.github.com>
2022-09-25 06:41:57 +0200

    Update agendarreuniao.php
    actualizacao da tela cadastro

--- views/agendarreuniao.php
+++ views/agendarreuniao.php
@@ -1,140 +1,110 @@
 <!DOCTYPE html>
-<html >
-<head>
+<html lang="en">
 
-<meta charset="utf-8"/>
+<head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aqui fica o seu Titulo</title>
-
- <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
- <link rel="stylesheet" href="style.css">
-
+    <title>Agendar Reunião</title>
+    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 
-
-    <body>
-
-
+<body>
     <div class="container-fluid">
-   
+        <div class="centro w-40">
+            <div class="w-40">
+                <div class="row">
+                    
 
-        <div class="col-sm-6 offset-sm-3 card bg-light p-1">
-
-
-            <h5>cSP</h5>
-
-            <h6>Nome do professor</h6>
-        </div>
-
-    
+                    <div class="telaP col-md-9 col-lg-9 col-sm-12 px-auto mx-auto">
 
 
+                    <div class="alinhamento">
+                            <div class="alinhamnento4">
+                               cSP 
+                            </div>
+                            <div class="alinhamento3">
+                                Nome do Professor
+                            </div>
 
-
-</div>
-
-
-
-                <div class="container-fluid">
-                    <div class="row mt-5">
-                        <div class="col-sm-6 offset-sm-3 card bg-light p-5">
-                            
-                            <h1 >Agendar Reunião</h1>
-
-
-                            
-                            <form action="controller/" method="post">
-                                <div class="mb-3">
-                                    
-                                    <input type="date" name="datatime"  class="form-control"id="" >
-                               
-                                   
-
-                                </div> 
-                                <div class="mb-3">
-                                   
-                                  <select name="Participantes" id="" class="form-select"  >
-
-                                        <option value="1">Selecione os participantes</option>
-                                        <option value="2"></option>
-                                        <option value="3"></option>
-                                        <option value="4"></option>
-                                        <option value="5"></option>
-                                        <option value="6"></option>
-                                        <option value="7"></option>
-                                        <option value="8"></option>
-                                        <option value="9"></option>
-
-
-
-                                     </select>
-
-
-
-                                </div>
-        
-                                <div class="mb-3">
-                                
-                                   
-
-                                     <input type="text" name="assunto" placeholder="Assunto da reunião" class="form-control"id="" >
-                            
-                                   
-
-                                </div> 
-        
-        
-                                <select name="Tipo de reunião" id="" class="form-select my-3">
-                                        <option value="1">Tipo de Reunião</option>
-                                        <option value="2">Urgente</option>
-                                        <option value="3">Trimestral</option>
-                                        <option value="4"></option>
-                                </select>
-
-
-                                <div class="class">
-                                    <div class="agendar">
-                                        <button type="button" class="btn btn-outline-success">Agendar</button>
-                                        <span>
-                                            <i class="fa-solid fa-calendar-days"></i>
-                                          </span> 
-                                    </div>
-                                    
-
-
-                                </div>
-                             
-
-
-                                         <div class="cancelar">
-
-
-                                    <button type="button" class="btn btn-outline-success">Cancelar</button>
-
-                                </div>
-
-                               
-
-
-
-
-                            </form>
-                        </div>
                     </div>
-                </div>
+            
+                        <h1 class="texto text-center mt-5">Agendar Reunião</h1>
+                        <form action="controller/" method="post" class=" row p-3">
+                            
+                <div class="alinhamento1" >
+
+                 
+<p></p>
+                    
+                            <div class=" col-md-4 col-lg-4 col-sm-12">
+                                <div class="input-group mb-3" style="width: 185%">
+                                    <span class="input-group-text"><i class="fas fa-user" ></i></span>
+                                    <input type="date" class="form-control" name="dete" placeholder="data da Reunião" style="width: 80%">
+                                </div>
+                            </div>
+
+<p></p>
+
+                            <div class="col-md-4 col-lg-4 col-sm-12">
+                                <div class="input-group mb-3" style="width: 185%">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    <select class="form-select" name="participantes" placeholder="Selecione os Participantes" style="width: 80%">
+                                        <option selected>Selecione Participantes</option>
+                                        <option>Turma A</option>
+                                        <option>Turma B</option>
+                                    </select>
+                                </div>
+                            </div>
+<p></p>
+                            
+                            <div class=" col-md-4 col-lg-4 col-sm-12">
+                                <div class="input-group mb-3" style="width: 185%">
+                                    <span class="input-group-text"><i class="fas fa-user" ></i></span>
+                                    <input type="text" class="form-control" name="assunto" placeholder="Assunto da Reunião" style="width: 80%">
+                                </div>
+                            </div>
+<p></p>
+
+                            <div class="col-md-4 col-lg-4 col-sm-12">
+                                <div class="input-group mb-3" style="width: 185%">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    <select class="form-select" name="tipo" style="width: 80%">
+                                        <option selected>Tipo de Reunião</option>
+                                        <option>Ordinaria</option>
+                                        <option>Extra-ordinaria</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            
+
+                    </div>
+                        
+<p></p>   
+<br>     
+                     
+                        
+                            <div class="col-md-3 col-lg-3 col-sm-12 botao text-center " style="margin-left: 25%">
+                                <button type="submit" class="btn"  >Agendar</button>
+                            </div>
+
+                            <div class="col-md-3 col-lg-3 col-sm-12 botao text-center mx-3" style="margin-left: 75%">
+                                <button class="btn" >Canselar</button>
+                            </div>
+                       
+                   
+                       
 
 
-
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
+</body>
 
-      
-       
-
-
-
-
-    </body>
 </html>
