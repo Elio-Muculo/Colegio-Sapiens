b6d23a70257fce0952fc33eb34f6b260a1ced731
Elliot <emuculo25@gmail.com>
2022-09-25 22:29:53 +0200

    fix login

--- controller/Controllerlogin.php
+++ controller/Controllerlogin.php
@@ -7,41 +7,55 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $error = [];
 
     $senha = $_POST['senha'];
-
     $data = [
-        $username = $_POST['nome']
+        'username' => $_POST['nome'],
     ];
 
-    //consulta para verificar se o user existe na bd
-    $sql = "SELECT * FROM Usuario WHERE username = ? ";
-
-
-    if (countRow($sql, $data) == 1) {
-
-        //pega os dados do usuario
+    //consulta para verificar se o user existe na bd e retornar 1 ou mais caso existe
+    $sql = "SELECT * FROM usuario WHERE username = :username";
+    $usuarioExiste = countRow($sql, $data);
+    if ($usuarioExiste > 0) {
+        //pega os dados do usuario existente
         $dados = readOne($sql, $data);
-
+        
         //senha vinda da bd
         $senhaUser = $dados['senha'];
 
-        //compara a senha introduzida com a da bd
-        if (password_verify($senha, $senhaUser)) {
+        // verifica se a senha da Db e igual inserida e retorna a senha caso verdade
+        // retorna null caso senhas incompativeis
+        $senhaVerificada = password_verify($senha, $senhaUser) ? $senhaUser : null;
+        
+        //se retornar null da senha significa senhas diferentes
+        // retornar para login com messagem de senhas erradas
+        if ($senhaVerificada != null) {
+            // chegou aqui porque a senha inserida e igual da base dados
+            // verificar se existe um usuario com nome, senha e estado activo na base dados
+            $sql = "SELECT * FROM usuario WHERE username = :username AND senha = :senha AND estado = :estado"; 
+            $data = [
+                'username' => $_POST['nome'],
+                'senha' => $senhaUser,
+                'estado' => 1
+            ];
 
-            //verifica se a conta esta activa
-            if ($dados['estado'] == 1) {
-
+            // se retornar um 1 significa que existe um user activo com 
+            // um determinado perfil 
+            $contaVerificada = countRow($sql, $data);
+            if ($contaVerificada == 1) {
+                // pegar os dados desse usuario para poder redirecionar no lugar certo
+                $dados = readOne($sql, $data);
+                //caso o perfil seja professor
                 if ($dados['perfil'] == 'professor') {
-                    //caso o perfil seja professor
-                    $_SESSION['user_id'] = $senhaUser;
+                    // sessao com o codigo(id) do professor
+                    $_SESSION['user_id'] = $dados['codigo'];
                     $_SESSION['user_permission'] = $dados['perfil'];
-                    header("location:../views/dashboardProfessor.php");
+                    header("location: ../views/dashboardProfessor.php");
                     die();
                 } else {
                     if ($dados['perfil'] == 'admin') {
                         //caso o perfil seja admin
                         $_SESSION['user_id'] = $senhaUser;
                         $_SESSION['user_permission'] = $dados['perfil'];
-                        header("location:../views/admin.php");
+                        header("location: ../views/admin.php");
                         die();
                     } else {
                         //caso o perfil seja encarregado
